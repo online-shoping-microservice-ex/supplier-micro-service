19b96d08a1357472fa9416832a62dc4cd5ad5a5d
kseshasai <kseshasai@SYS-66.mshome.net>
2020-02-18 19:46:20 +0530

    initial set up

---
+++ src/main/java/com/online/microservice/supplierservice/SupplierMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.online.microservice.supplierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/online/microservice/supplierservice/SupplierMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.microservice.supplierservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
